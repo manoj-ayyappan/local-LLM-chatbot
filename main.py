ac609d0e8192918272eb9a941606519ad73040b4
manoj-ayyappan <manojayyappan18@gmail.com>
2025-01-23 11:31:04 -0600

    Using the lanchain module for queries

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+template = """
+Answer the question below.
+
+Here is the conversation history: {context}
+
+Question: {question}
+
+Answer: 
+"""
 
 model = OllamaLLM(model="llama3.2")
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
 
-result = model.invoke(input="hello world")
+result = chain.invoke({"context":"", "question": "Who is the president of the US?"})
 print(result)
